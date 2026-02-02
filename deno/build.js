60e364313b1f7ea19a2e1e79f50dd3bcf527e8f7
Jan van Hellemond <jan@jvhellemond.nl>
2026-02-02 01:07:14 +0100

    Debugging.

--- deno/build.js
+++ deno/build.js
@@ -31,6 +31,10 @@ const contentCache = {};
 
 // ⚠️ These variables are publicly exposed!
 const publicEnvKeys = (Deno.env.get("PUBLIC_ENV") ?? "").split(" ");
+
+// @debug:
+console.log(Deno.env.get("PUBLIC_ENV"));
+
 const publicEnv = Object.fromEntries(publicEnvKeys.map(key => [key, Deno.env.get(key)]));
 
 const esbuildOptions = {
